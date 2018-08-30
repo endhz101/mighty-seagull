9b3a5a9c835aa796fb5e030017895b193672212d
Tornz <cantornaeric2024@gmail.com>
2018-08-30 15:49:55 +0800

    Update Nav Bar

--- src/app/shared/shared.component.ts
+++ src/app/shared/shared.component.ts
@@ -1,6 +1,8 @@
+
 declare function require(path: string);
 import { Component, OnInit, Renderer } from '@angular/core';
 import { Router } from '@angular/router';
+import { NavUrl } from '../model/navurl.model';
 
 
 @Component({
@@ -9,22 +11,39 @@ import { Router } from '@angular/router';
   styleUrls: ['./shared.component.css']
 })
 export class SharedComponent implements OnInit {
-  logo=require("../../images/pccwlogo1.png"); 
-  userDefaultImage=require("../../images/defaultimage.png"); 
+  logo = require("../../images/pccwlogo1.png");
+  userDefaultImage = require("../../images/defaultimage.png");
+  listNavUrl: NavUrl[] = [
+    new NavUrl(0, "Project Summary", "active"),
+    new NavUrl(1, "FPA", ""),
+    new NavUrl(2, "Implementation", ""),
+    new NavUrl(3, "Project Estimation", ""),
+    new NavUrl(4, "Cost Model", "")
+  ]
 
-  constructor(private router:Router,private render:Renderer) { }
+  constructor(private router: Router, private render: Renderer) { }
 
   ngOnInit() {
   }
-  gotoProjectSummary()
-  {
-    this.router.navigate(['/projectsummary']);    
+
+  routeTo(item: NavUrl) {
+
+
+    switch (item.navUrlId) {
+      case 0: {
+        this.router.navigate(['/projectsummary']);
+        break;
+      }
+      case 4: {
+        this.router.navigate(['/costmodel']);
+        break;
+      }
+    }
+    this.updateMyClass(item);
   }
-  gotoCostModel()
-  {
-    this.router.navigate(['/costmodel']);    
-  }
-  gotoFpa(){
-    this.router.navigate(['/fpa']);
+  updateMyClass(item: NavUrl) {
+    const checkActive = this.listNavUrl.find(x => x.myclass == "active");
+    this.listNavUrl[checkActive.navUrlId].myclass = "";
+    this.listNavUrl[item.navUrlId].myclass = "active";
   }
 }
