1bab4f1714d2c4a6f6ce4c441b5f1c551f1f37a5
Tornz <cantornaeric2024@gmail.com>
2018-08-30 11:04:08 +0800

    add sample model

---
+++ src/app/model/frontend.model.ts
@@ -0,0 +1,10 @@
+export class Frontend {
+    public frontendId: number;
+    public description: string;
+
+    constructor(frontendId: number, description: string) {
+        this.frontendId = frontendId;
+        this.description = description;
+    }
+
+}
