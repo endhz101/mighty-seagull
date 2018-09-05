e2c5cc76fba78d9b89563638f37ec14e81aa9506
maricorpalangao <maricorpalangao.pccw@gmail.com>
2018-09-05 13:11:57 +0800

    separated model from controller

---
+++ src/app/model/costmodel.model.ts
@@ -0,0 +1,13 @@
+export class Costmodel{
+  public onsite: string;
+  public role: string;
+  public grade: string;
+  public module: string;
+
+  constructor(onsite: string, role: string, grade: string, module: string, module1: string ){
+    this.onsite = onsite;
+    this.role = role;
+    this.grade = grade;
+    this.module = module;
+  }
+}
