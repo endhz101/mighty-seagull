cb204923c9f67d62d0aae13d583d2b82bcda9bd2
CORP\80978679 <juanhenryi.yadao@pccw.com>
2018-09-06 17:04:44 +0800

    add model for implementation module, fixe minor UI bugs on

--- src/app/data-services/implementation.services.ts
+++ src/app/data-services/implementation.services.ts
@@ -1,40 +1,50 @@
 
-import { HttpClient, HttpParams} from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from "../../../node_modules/@angular/core";
 import { environment } from '../../environments/environment';
+import { ProjectResourcePlans } from '../model/project-resource-plans.model';
+
 // import { environment } from '../../environments/environment';
 
 
 @Injectable()
 export class ImplementationService {
     // data = [];
+    loader: any = null
     constructor(private http: HttpClient) {
-        
-            
+
+
     }
-    getImplementation(){
-        return this.http.get<any[]>(environment.REST_API_URL + 'api/ProjectResourcePlans')
-        .toPromise();
+    getImplementation() {
+        try {
+            return this.http.get<Array<ProjectResourcePlans>>(environment.REST_API_URL + 'api/ProjectResourcePlans')
+                .toPromise().catch(e => { throw e });
+        } catch (error) {
+            console.error("error: ", error)
+        }
+        finally {
+        }
+
     }
 
-    getImplementationbyId(id: any){
+    getImplementationbyId(id: any) {
         return this.http.get<any[]>(environment.REST_API_URL + 'api/ProjectResourcePlans/' + 1)
-        .toPromise();
+            .toPromise();
     }
 
-    postNewImplmementation(data: any[]){
+    postNewImplmementation(data: ProjectResourcePlans) {
         return this.http.post<any[]>(environment.REST_API_URL + 'api/ProjectResourcePlans/', data)
-        .toPromise();
+            .toPromise();
     }
 
-    deleteImplementation(id: any){
+    deleteImplementation(id: any) {
         return this.http.delete<any[]>(environment.REST_API_URL + 'api/ProjectResourcePlans/' + 1)
-        .toPromise();
+            .toPromise();
     }
 
-    getProjectPlanbySummary(id: any){
-        return this.http.get<any[]>(environment.REST_API_URL + 'api/ProjectResourcePlans/'+ 1)
-        .toPromise();
+    getProjectPlanbySummary(id: any) {
+        return this.http.get<any[]>(environment.REST_API_URL + 'api/ProjectResourcePlans/' + 1)
+            .toPromise();
     }
 
     // editImplementation(data: any[]){
